2d024e2bad1843a7662b10640ac4b485000bc564
Lee McCormick <lee.mccormick@dialexa.com>
2023-09-26 12:27:12 -0500

    try to added env file to section 9

--- The_Complete_React_Native_Hooks_Course/09-section-9-using-outside-api's/section-9-food-app-api/App.js
+++ The_Complete_React_Native_Hooks_Course/09-section-9-using-outside-api's/section-9-food-app-api/App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Search',
     defaultNavigationOptions: {
-      title: 'Business UseEffect Search',
+      title: 'Business api Search',
     },
   }
 );
